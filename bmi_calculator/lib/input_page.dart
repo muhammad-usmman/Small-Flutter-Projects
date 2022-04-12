d2adff34651b22715c4d543962cd054c139f1989
muhammad-usmman <101857162+muhammad-usmman@users.noreply.github.com>
2022-04-12 22:04:33 +0500

    Repeat Text and Icon Widget

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,7 +1,9 @@
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
+
 
   @override
   State<InputPage> createState() => _InputPageState();
@@ -21,21 +23,35 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child:  RepeatContainerCode(
                   colors: Color(0xFF1D1E33),
+                  cardwidget: RepeatTextAndIconWidget(
+                    iconData: FontAwesomeIcons.male,
+                    label:'MALE',
+                  ),
                 ),
               ),
-              Expanded(child:  RepeatContainerCode( colors: Color(0xFF1D1E33),),
+              Expanded(
+                child:  RepeatContainerCode(
+                  colors: Color(0xFF1D1E33),
+                cardwidget: RepeatTextAndIconWidget(
+                    iconData: FontAwesomeIcons.female,
+                    label:'FEMALE',
+                ),
+                ),
               ),
 
             ],
           ),
           ),
-          Expanded(child:  RepeatContainerCode( colors: Color(0xFF1D1E33),),
+          Expanded(child:
+          RepeatContainerCode( colors: Color(0xFF1D1E33),),
              ),
           Expanded(child: Row(
             children:<Widget> [
-              Expanded(child:  RepeatContainerCode( colors: Color(0xFF1D1E33),),
+              Expanded(
+                child: RepeatContainerCode( colors: Color(0xFF1D1E33),),
               ),
-              Expanded(child: RepeatContainerCode(   colors: Color(0xFF1D1E33),),),
+              Expanded(
+                child: RepeatContainerCode(   colors: Color(0xFF1D1E33),),),
 
             ],
            ),
@@ -46,15 +62,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class RepeatContainerCode extends StatelessWidget {
-   RepeatContainerCode({@required this.colors});
- final Color? colors;
+class RepeatTextAndIconWidget extends StatelessWidget {
+  RepeatTextAndIconWidget({ required this.iconData, required this.label});
+final IconData iconData;
+final String label;
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+     children: [
+       Icon(
+         iconData,
+         size: 85.0,
+       ),
+       SizedBox(
+         height: 15.0,
+       ),
+       Text(
+        label,
+         style: TextStyle(
+         fontSize: 22.0,
+         color: Color(0xFF8D8E98)
+       ),)
+
+     ],
+
+    );
+  }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+   RepeatContainerCode({@required this.colors,this.cardwidget});
+ final Color? colors;
+ final Widget? cardwidget;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardwidget,
       decoration: BoxDecoration(
         color: colors,
             borderRadius: BorderRadius.circular(10.0)
