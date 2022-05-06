9f5456632e8c7fd1a875df54a19a23beb3ed6cfc
muhammad-usmman <101857162+muhammad-usmman@users.noreply.github.com>
2022-05-06 09:18:14 +0500

    start
    splash screen

---
+++ lab_mid_tasbeeh/android/app/src/main/kotlin/com/example/lab_mid_tasbeeh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab_mid_tasbeeh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab_mid_tasbeeh/lib/screens/LoadingScreen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+
+
+
+
+
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                SecondScreen()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("Tasbeeh Counter")),
+      body: Center(
+          child:Text("Home page",textScaleFactor: 2,)
+      ),
+    );
+  }
+}
