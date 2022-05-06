43f8f2957620087392ca34ef46d1b57481f4d039
muhammad-usmman <101857162+muhammad-usmman@users.noreply.github.com>
2022-05-06 13:18:58 +0500

    background image and drawer

--- lab_mid_tasbeeh/lib/screens/homepage.dart
+++ lab_mid_tasbeeh/lib/screens/homepage.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:lab_mid_tasbeeh/utilities/constants.dart';
 
-class Homepage extends StatefulWidget {
-  @override
-  _HomepageState createState() => _HomepageState();
-
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/backgroundimage.jpeg'),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.white.withOpacity(0.8), BlendMode.dstATop),
-          ),
-        ),
-        constraints: BoxConstraints.expand(),
-
-      ),
-    );
-  }
-}
+// class Homepage extends StatefulWidget {
+//   @override
+//   _HomepageState createState() => _HomepageState();
+//
+// }
+//
+// class _HomepageState extends State<Homepage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('images/backgroundimage.jpeg'),
+//             fit: BoxFit.cover,
+//             colorFilter: ColorFilter.mode(
+//                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
+//           ),
+//         ),
+//         constraints: BoxConstraints.expand(),
+//
+//       ),
+//     );
+//   }
+// }
 
 
 class home extends StatefulWidget {
@@ -43,6 +43,18 @@ class _homeState extends State<home> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
+        body:Container(
+          constraints: BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image:AssetImage('images/backgroundimage.jpeg'),
+                fit: BoxFit.cover),
+          ),
+          child: const Text(
+            'Image in fullscreen',
+            style: TextStyle(fontSize: 34, color: Colors.red),
+          ),
+        ),
         drawer: Drawer( child: ListView(
 
           padding: EdgeInsets.zero,
@@ -57,7 +69,7 @@ class _homeState extends State<home> {
             FlatButton(
 
               onPressed:(){
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Homepage()));
+                Navigator.push(context,MaterialPageRoute(builder: (context) =>home()));
               },
               child: Text("HOME",style: TextStyle(color: Colors.teal)),
             ),
@@ -88,23 +100,15 @@ class _homeState extends State<home> {
 
         backgroundColor: Colors.teal,
 
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-
-          ),
-        ),
+        // body: SafeArea(
+        //   child: Padding(
+        //     padding: EdgeInsets.symmetric(horizontal: 10.0),
+        //
+        //   ),
+        // ),
       ),
-    body: Container(
-    decoration: BoxDecoration(
-    image: DecorationImage(
-    image: AssetImage('images/backgroundimage.jpeg'),
-    fit: BoxFit.cover,
-    colorFilter: ColorFilter.mode(
-    Colors.white.withOpacity(0.8), BlendMode.dstATop),
-    ),
-    ),
-    ));
+
+    );
 
   }
 }
